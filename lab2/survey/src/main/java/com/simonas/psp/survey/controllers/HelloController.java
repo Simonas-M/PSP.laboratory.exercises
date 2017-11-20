67ab9dca51dd4b2a064cca4646e85ec26c585cd9
Simonas Mikulis <simonas.mikulis+gitkraken@gmail.com>
2017-11-20 08:03:47 +0200

    created initial spring project

---
+++ lab2/survey/src/main/java/com/simonas/psp/survey/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.simonas.psp.survey.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
